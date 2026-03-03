85e7c5401d21427635857291f6e278b33c0b3aa4
Kiril Bormin <borminnkiril@gmail.com>
2026-03-03 09:22:01 +0100

    feat (home) : no chat selected section

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,35 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import { axiosInstance } from "../lib/axios";
+
+export const useChatStore = create((set) => ({
+  messages: [],
+  users: [],
+  selectedUser: null,
+  isUsersLoading: false,
+  isMessagesLoading: false,
+
+  getUsers: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const res = await axiosInstance.get("/messages/users");
+      set({ users: res.data });
+    } catch (error) {
+      toast.error("Failed to load users. Please try again.");
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
+  getMessages: async (UserId) => {
+    set({ isMessagesLoading: true });
+    try {
+      const res = await axiosInstance.get(`/messages/${UserId}`);
+    } catch (error) {
+      toast.error("Failed to load messages. Please try again.");
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+  //optimiser plus tard
+  setSelectedUser: (selectedUser) => set({ selectedUser }),
+}));
